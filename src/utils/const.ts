6c47a85bdfad9472edb42621c0049f8be8acadac
Niewei <neewii.yang@outlook.com>
2026-01-16 20:50:05 +0800

    改颜色

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -200,6 +200,7 @@ const purple2 = 'rgb(127, 34, 254)';
 const veryPeri = 'rgb(105,106,173)'; //长春花蓝
 const red = 'rgb(255,0,0)'; //大红色
 const orange = 'rgb(255,153,51)'; //大红色
+const blue = 'rgb(71,184,224)';
 
 // If your map has an offset please change this line
 // issues #92 and #198
@@ -234,8 +235,8 @@ export const getRuntimeSingleColor = (
 };
 
 // Legacy export for backwards compatibility
-export const RUN_COLOR = [yellow, yellow_compl];
-export const RIDE_COLOR = [green, green];
+export const RUN_COLOR = [yellow, blue];
+export const RIDE_COLOR = [green, orange];
 export const VIRTUAL_RIDE_COLOR = [veryPeri, veryPeri];
 export const HIKE_COLOR = [pink, pink];
 export const SWIM_COLOR = [gold, gold];
