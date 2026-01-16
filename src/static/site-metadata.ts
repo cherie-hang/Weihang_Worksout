09fc162cc01e32fdf7937e88ed1421599ea62152
Niewei <neewii.yang@outlook.com>
2026-01-16 09:06:22 +0800

    个人化修改

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -16,7 +16,7 @@ const getBasePath = () => {
 };
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Workouts Map',
+  siteTitle: 'Neewii的Workouts Map',
   siteUrl: 'https://ben29.xyz',
   logo: 'https://avatars3.githubusercontent.com/u/6956444?s=460&u=97e1062227c4088db3c987146455245c6fa78441',
   description: 'Personal site and blog',
