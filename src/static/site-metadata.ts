a34cfeb7c492d73c6b1780700748904d0b677fb5
Niewei <neewii.yang@outlook.com>
2026-01-16 09:45:18 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -17,8 +17,8 @@ const getBasePath = () => {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'Neewii的Workouts Map',
-  siteUrl: 'https://ben29.xyz',
-  logo: 'https://avatars3.githubusercontent.com/u/6956444?s=460&u=97e1062227c4088db3c987146455245c6fa78441',
+  siteUrl: 'neewii-worksout.vercel.app',
+  logo: 'https://avatars.githubusercontent.com/u/138655239?v=4',
   description: 'Personal site and blog',
   keywords: 'workouts, running, cycling, riding, roadtrip, hiking, swimming',
   navLinks: [
@@ -27,7 +27,7 @@ const data: ISiteMetadataResult = {
       url: `${getBasePath()}/summary`,
     },
     {
-      name: 'Blog',
+      name: 'ben的Blog',
       url: 'https://ben29.xyz',
     },
     {
